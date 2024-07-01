b39cb396c9edbe5b98a03e36f5b8d16b7a585642
Johan Salazar <johanmsalazarr@gmail.com>
2024-07-01 10:16:17 -0400

    adding files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scheduled Page</title>
+</head>
+<body>
+    <?php
+    $dayofweek = date("l");
+    ?>
+    <p><?php echo $dayofweek?></p>
+</body>
+</html>
